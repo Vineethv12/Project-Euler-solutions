00d795b56fad549fdef2feef9e6008471af9dcdb
Vineeth V <72489940+Vineethv12@users.noreply.github.com>
2020-10-11 19:33:03 +0530

    Add files via upload

---
+++ Problems/Problem2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+numbers = [1,2]
+sum = 0
+for i in range(4000000):
+    if (i==numbers[-1]+numbers[-2]):
+        numbers.append(i)
+    
+for j in numbers:
+    if (j%2 == 0):
+        sum += j
+    
+print(sum)
+
